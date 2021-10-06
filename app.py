8fa8e97d501c1f11a636e898793c2bcafaf6c0cf
Ignx <58445918+ignxal@users.noreply.github.com>
2021-10-06 19:31:09 -0300

    Update
    Looking forward to extract more data from the API

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 import os
 from dotenv import load_dotenv
 from get_TodayUrl import get_TodayUrl
-from get_Timesheet import get_Timesheet
+from get_Timesheet import dataToExcel, get_Folders, get_Lists, get_Spaces, get_Tasks, get_Timesheet
 
 load_dotenv()
 my_headers = {'Authorization' : os.getenv('TOKEN_VAR')}
-today_url = get_TodayUrl()
-get_Timesheet(today_url, my_headers)
+timesheet = get_Timesheet(get_TodayUrl(), my_headers)
+spaces = get_Spaces(my_headers)
+id_spaces = [d.get('id') for sublists in spaces.values() for d in sublists]
+
+i=0
+while i < len(id_spaces):
+    #Traigo los folders del space y obtengo sus ID
+    folders = get_Folders(id_spaces[i], my_headers)
+    id_folders = [d.get('id') for sublists in folders.values() for d in sublists]
+    #Valido empty
+    if id_folders != []:
+        x=0
+        while x < len(id_folders):
+            #Traigo Lists de cada Folder y obtengo sus ID
+            lists = get_Lists(id_folders[x], my_headers)
+            id_lists = [d.get('id') for sublists in lists.values() for d in sublists]
+            #Valido empty
+            if id_lists != []: 
+                y=0
+                while y < len(id_lists):
+                    #Traigo Tasks de cada Lists y obtengo sus ID
+                    tasks = get_Tasks(id_lists[y], my_headers)
+                    id_tasks = [d.get('id') for sublists in tasks.values() for d in sublists]
+                    #Hacer merge en esta linea con timesheet
+
+#Data a excel
+#dataToExcel(mergeHecho, my_headers)
--- get_Timesheet.py
+++ get_Timesheet.py
@@ -4,10 +4,27 @@ from pandas import json_normalize
 
 def get_Timesheet(url, myh):
     try:
-        Timesheet = json.loads(requests.get(url, headers=myh).text)
+        timesheet = json.loads(requests.get(url, headers=myh).text)
+        return timesheet
     except:
         print("URL invalida")
-    dataToExcel(Timesheet)
+
+def get_Spaces(myh):
+    spaces = json.loads(requests.get('https://api.clickup.com/api/v2/team/{TeamID}/space?archived=False', headers=myh).text)
+    return spaces
+
+def get_Folders(space_id, myh):
+    folders = json.loads(requests.get('https://api.clickup.com/api/v2/space/' + space_id + '/folder?archived=False', headers=myh).text)
+    return folders
+
+def get_Lists(folder_id, myh):
+    lists = json.loads(requests.get('https://api.clickup.com/api/v2/folder/' + folder_id + '/list?archived=False', headers=myh).text)
+    return lists
+
+def get_Tasks(tasks_id, myh):
+    #faltar configurar params del link
+    tasks = json.loads(requests.get('https://api.clickup.com/api/v2/list/' + tasks_id + '/task?archived=&page=&order_by=&reverse=&subtasks=&statuses%5B%5D=&include_closed=&assignees%5B%5D=&due_date_gt=&due_date_lt=&date_created_gt=&date_created_lt=&date_updated_gt=&date_updated_lt=&custom_fields%5B%5D=', headers=myh).text)
+    return tasks
 
 def dataToExcel(Timesheet):
     data = json_normalize(Timesheet, record_path =['data'])
